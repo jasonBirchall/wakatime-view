b9fe840f4873f8faeedd79aeed64f6395c44aafe
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-09-15 17:31:32 +0100

    :white_check_mark: Add unit test for successful config setup

--- test_view.py
+++ test_view.py
@@ -1,35 +1,31 @@
-import pytest
-from unittest.mock import Mock, patch
-from view import today, setup  # importing from your script named view.py
+import os
+import shutil
+import unittest
+from typer.testing import CliRunner
+import view
 
-# Test for the "today" command
-def test_today():
-    with patch('view.get_api_key', return_value="test_api_key"), \
-         patch('requests.get') as mock_get:
-        
-        # Mock the API response
-        mock_get.return_value.status_code = 200
-        mock_get.return_value.json.return_value = {'data': {'grand_total': {'text': '5 hrs'}}}
-        
-        # Call the "today" command
-        today()
-        
-        # Assert that the API was called with the correct endpoint and headers
-        mock_get.assert_called_once_with(
-            'https://wakatime.com/api/v1/users/current/status_bar/today', 
-            headers={'Authorization': 'Basic dGVzdF9hcGlfa2V5'}
-        )
 
 # Test for the "setup" command
-def test_setup():
-    with patch('typer.prompt', return_value="test_api_key"), \
-         patch('builtins.open', new_callable=Mock) as mock_open, \
-         patch('toml.dump') as mock_dump, \
-         patch('view.CONFIG_FILE.is_file', return_value=False):
-        
-        # Call the "setup" command
-        setup()
-        
-        # Assert that the config file was written with the correct content
-        mock_open.assert_called_once_with(view.CONFIG_FILE, 'w')  # using view.CONFIG_FILE directly
-        mock_dump.assert_called_once_with({'wakatime': {'api_key': 'test_api_key'}}, mock_open())
+class TestSetupCommand(unittest.TestCase):
+    def setUp(self):
+        self.runner = CliRunner()
+        path = view.CONFIG_FILE.__str__()
+        if os.path.exists(view.CONFIG_FILE):
+            shutil.move(path, path + ".bak")
+
+    def tearDown(self):
+        path = view.CONFIG_FILE.__str__()
+        if os.path.exists(path):
+            os.remove(path)
+            if os.path.exists(path + ".bak"):
+                shutil.move(path + ".bak", path)
+
+    def test_create_config_file(self):
+        result = self.runner.invoke(view.app, ["setup"], input="123456789")
+        assert result.exit_code == 0
+        print(result.output)
+        assert "Config file created. Please edit the file and add your API key." in result.output
+
+
+if __name__ == "__main__":
+    unittest.main()
